c3c09e4f63ef91f40421d87bc4ea5324c9b6cea9
André Daniel <andre.fervilha@gmail.com>
2018-04-18 10:02:18 -0300

    Corrigindo git

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// css
+import '../scss/main.scss';
+
+// js
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'owl.carousel2';
+import './global-svg.js';
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:7
+            }
+        }
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+/**
+ * Assets Config file
+ */
+
+process.noDeprecation = true;
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('webpack-uglifyes-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+const config = {
+  entry: {
+    app: './src/js/main.js'
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader']
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { name: 'images/[name].[hash:6].[ext]', publicPath: '../', limit: 8192 }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { name: 'fonts/[name].[hash:6].[ext]', publicPath: '../', limit: 8192 }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+    new BrowserSyncPlugin({
+      files: [],
+      ghostMode: {
+        clicks: false,
+        location: false,
+        forms: false,
+        scroll: false
+      },
+      injectChanges: true,
+      logFileChanges: true,
+      logLevel: 'debug',
+      logPrefix: 'wepback',
+      notify: true,
+      reloadDelay: 0
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    }),
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new UglifyJSPlugin(),
+    new OptimizeCssAssetsPlugin()
+  );
+}
+
+module.exports = config;
