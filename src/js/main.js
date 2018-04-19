45d7eaaef424abe72271eb9a5ed5924c928f125b
André Daniel <andre.fervilha@gmail.com>
2018-04-19 19:09:45 -0300

    Melhorias no carousel

--- src/js/main.js
+++ src/js/main.js
@@ -21,16 +21,10 @@ $(document).ready(function(){
             '<i class="fa fa-angle-right"></i>'
         ],
         responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 7
-            }
+            0: { items: 1.75, nav: false },
+            600: { items: 3 },
+            1000: { items: 5 },
+            1500: { items: 7 }
         }
     });
 });
