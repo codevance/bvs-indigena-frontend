2b16275182bb250d6375526d5f938eca6ed1756a
DESKTOP-D97LQ97\jluca <jlucaspaixao@outlook.com>
2018-04-18 23:42:27 -0300

    Desenvolvendo o Mobile

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
         ],
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                nav: false
             },
             600: {
                 items: 3
